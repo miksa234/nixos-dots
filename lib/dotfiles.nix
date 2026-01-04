b6bf9e6c8cca17aadd8dcbad09b1f23d955b02a9
Milutin Popovic <milutin@popovic.xyz>
2026-01-04 19:36:34 +0000

    update dots

--- lib/dotfiles.nix
+++ lib/dotfiles.nix
@@ -2,6 +2,6 @@
   dotfiles = builtins.fetchGit {
     url = "git://popovic.xyz/dots.git";
     ref = "master";
-    rev = "87e0d4f41b10d31839a9edc96d8c16262d5ee686";
+    rev = "fbaea6517ddf5c0ca2a3f3c41d0e61b5014adbfd";
   };
 }
