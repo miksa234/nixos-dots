004d73d2624cba68919ba769dfc398c00f194de1
Milutin Popovic <milutin@popovic.xyz>
2026-03-07 18:16:04 +0100

    macos config

--- system/host/mac.nix
+++ system/host/mac.nix
@@ -44,7 +44,106 @@
     };
   };
 
-  system.primaryUser = "mika";
+  system = {
+    primaryUser = "mika";
+    defaults = {
+      controlcenter = {
+        BatteryShowPercentage = true;
+        NowPlaying = false;
+      };
+      CustomUserPreferences = {
+        "com.apple.symbolichotkeys" = {
+          "64" = {
+            # Disable 'Cmd + Space' for Spotlight Search
+            enabled = false;
+          };
+          "65" = {
+            # Disable 'Cmd + Alt + Space' for Finder search window
+            enabled = false;
+          };
+          "238" = {
+            # Set 'Control + Command + C' to center focused window
+            enabled = true;
+            value = {
+              parameters = [
+                99
+                8
+                1310720
+              ];
+              type = "standard";
+            };
+          };
+          "98" = {
+            # Disable 'Show Help menu'
+            enabled = false;
+            value = {
+              parameters = [
+                47
+                44
+                1179648
+              ];
+              type = "standard";
+            };
+          };
+        };
+      };
+      NSGlobalDomain = {
+        "com.apple.sound.beep.volume" = 0.000;
+        AppleInterfaceStyle = "Dark";
+        ApplePressAndHoldEnabled = false;
+        AppleShowAllExtensions = true;
+        InitialKeyRepeat = 20;
+        KeyRepeat = 2;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSAutomaticWindowAnimationsEnabled = false;
+        NSDocumentSaveNewDocumentsToCloud = false;
+        NSNavPanelExpandedStateForSaveMode = true;
+        PMPrintingExpandedStateForPrint = true;
+      };
+      trackpad = {
+        TrackpadRightClick = true;
+        TrackpadThreeFingerDrag = true;
+        Clicking = true;
+      };
+      finder = {
+        AppleShowAllFiles = true;
+        CreateDesktop = false;
+        FXDefaultSearchScope = "SCcf";
+        FXEnableExtensionChangeWarning = false;
+        FXPreferredViewStyle = "Nlsv";
+        QuitMenuItem = true;
+        ShowPathbar = true;
+        ShowStatusBar = true;
+        _FXShowPosixPathInTitle = true;
+        _FXSortFoldersFirst = true;
+      };
+      dock = {
+        autohide = true;
+        expose-animation-duration = 0.15;
+        show-recents = false;
+        showhidden = true;
+        persistent-apps = [ ];
+        tilesize = 30;
+        wvous-bl-corner = 1;
+        wvous-br-corner = 1;
+        wvous-tl-corner = 1;
+        wvous-tr-corner = 1;
+      };
+    };
+    keyboard = {
+      enableKeyMapping = true;
+      # Remap §± to ~
+      userKeyMapping = [
+        {
+          HIDKeyboardModifierMappingDst = 30064771125;
+          HIDKeyboardModifierMappingSrc = 30064771172;
+        }
+      ];
+    };
+  };
 
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
@@ -62,14 +161,17 @@
     wireguard-tools
   ];
 
+  services.aerospace = {
+    enable = true;
+    settings = {
+      gaps.inner.horizontal = 10;
+    };
+  };
+
 
   homebrew = {
     enable = true;
     prefix = "/opt/homebrew";
-    taps = [
-      "homebrew/cask"
-      "homebrew/cask-fonts"
-    ];
     brews = [
       "fzf"
       "ripgrep"
