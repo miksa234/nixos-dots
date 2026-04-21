0526ac4e7f2eed0cdaa1da954632da738132acd6
Milutin Popovic <milutin@popovic.xyz>
2026-04-21 15:26:00 +0100

    update deps

--- modules/dotfiles.nix
+++ modules/dotfiles.nix
@@ -2,13 +2,13 @@
   dotfiles = builtins.fetchGit {
     url = "git://popovic.xyz/dots.git";
     ref = "master";
-    rev = "16f7908b64b131dca39309e7e82b3ca364dcd187";
+    rev = "3f09f5d0eb88572f0f31cbfe97a48b05d3bfdb27";
 
   };
   nvim-config = builtins.fetchGit {
     url = "git://popovic.xyz/config.nvim.git";
     ref = "master";
-    rev = "59b8bcec9126fd8a0ca62aecb2c40dcf90175793";
+    rev = "678c7992ad31174a825c896cd724b3a6c155a87c";
 
   };
 }
